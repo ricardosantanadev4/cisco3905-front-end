931cb90a8835d07ce1f3221b0e95bda6ab02fc20
ricardosantanadev <ricardosantanadev@gmail.com>
2023-02-22 11:40:27 -0300

    update

--- src/app/ramais/services/ramais.service.ts
+++ src/app/ramais/services/ramais.service.ts
@@ -8,8 +8,9 @@ import { Ramais } from '../model/ramais';
 })
 
 export class RamaisService {
-  ramais: Ramais[] = [{ id: '1', ramal: '6001', serialNumber: 'lzk606895', ipCentral: '192.168.10.1', status: 'Disponível' }];
-  private readonly API = '/api/ramais-list';
+  // ramais: Ramais[] = [{ id: '1', ramal: '6001', serialNumber: 'lzk606895', ipCentral: '192.168.10.1', status: 'Disponível' }];
+  private readonly API = 'http://localhost:3000/values';
+  // private readonly API = '/api/ramais-list';
 
   constructor(private httpClient: HttpClient) {
   }
